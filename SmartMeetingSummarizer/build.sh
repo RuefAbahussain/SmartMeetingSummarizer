ec4c06e50becc118f74ed58ead707012ac8adf02
R.Abahussain <19413747ksa@aou.edu.sa>
2026-07-13 02:43:44 +0300

    Delete SmartMeetingSummarizer/build.sh

--- SmartMeetingSummarizer/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# Install Python dependencies
-pip install -r backend/requirements.txt
-
-# Install system dependencies required by python-magic and ffmpeg
-apt-get update
-apt-get install -y libmagic1 ffmpeg
